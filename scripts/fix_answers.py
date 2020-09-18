ae5d352e153f0330b32b2b490ac9f2d876497643
James Tanner <tanner.jc@gmail.com>
2020-09-19 01:45:05 +0200

    checkin

---
+++ scripts/fix_answers.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import glob
+import json
+import os
+
+
+def main():
+    dpath = os.path.join('react.quizzer.app', 'server', 'data', 'courses', 'C960_discrete_math_II')
+    jfiles = glob.glob('%s/*.json' % dpath)
+
+    BAD = set()
+
+    for jf in jfiles:
+        print(jf)
+        if not jf:
+            continue
+        changed = False
+        with open(jf, 'r') as f:
+            jd = json.loads(f.read())
+        if not jd:
+            BAD.add(jf)
+        if jd.get('answer') is None:
+            BAD.add(jf)
+            continue
+        print(jd['answer'])
+        if isinstance(jd['answer'], int):
+            continue
+        if 'because' in jd['answer']:
+            #jd['answer'] = jd['answer'].split('because')[0].strip()
+            #changed = True
+            pass
+
+        if ' or ' in jd['answer']:
+            import epdb; epdb.st()
+
+    import epdb; epdb.st()
+
+
+if __name__ == "__main__":
+    main()
